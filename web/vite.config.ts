11f3e245949015447962a33de4719e1cb773b7ca
Rono <rowanpsmith@yahoo.com.au>
2026-06-20 22:48:10 +1000

    Fix Relative Paths

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Spotify-Extended-Streaming-History-Analysis/',
+  base: './',
   build: {
     outDir: '../docs',
     emptyOutDir: true,
